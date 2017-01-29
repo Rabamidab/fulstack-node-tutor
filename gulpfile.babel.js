f8c073976f5e85f3a723db483624a488d82780d2
Unknown <milesofforgottenlove@gmail.com>
2017-01-29 12:13:34 +0300

    add lint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,11 @@ import gulp from 'gulp';
 import babel from 'gulp-babel';
 import del from 'del';
 import { exec } from 'child_process';
+import eslint from 'gulp-eslint';
 
 const paths = {
   allSrcJs: 'src/**/*.js',
+  gulpFile: 'gulpfile.babel.js',
   libDir: 'lib',
 };
 
@@ -12,7 +14,7 @@ gulp.task('clean', () => {
   return del(paths.libDir);
 });
 
-gulp.task('build', ['clean'], () => {
+gulp.task('build', ['lint', 'clean'], () => {
   return gulp.src(paths.allSrcJs)
     .pipe(babel())
     .pipe(gulp.dest(paths.libDir));
@@ -27,6 +29,16 @@ gulp.task('main', ['build'], (callback) => {
 
 gulp.task('watch', () => {
   gulp.watch(paths.allSrcJs, ['main']);
+});
+
+gulp.task('lint', () => {
+  return gulp.src([
+    paths.allSrcJs,
+    paths.gulpFile,
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('default', ['watch', 'main']);
